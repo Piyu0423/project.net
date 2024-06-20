3645ebd3bdb02eb44de8571e9fc0966d7c45a254
Piyu0423 <piumikamadubashini5@gmail.com>
2024-06-20 15:03:17 +0800

    Student Entity

--- project.net/Data/ApplicationDbContext.cs
+++ project.net/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using project.net.Models.Entities;
 
 namespace project.net.Data
 {
@@ -6,7 +7,9 @@ namespace project.net.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options) 
         {
+
         }
+        public DbSet<Student> Students { get; set; }
 
     }
 }
